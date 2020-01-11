7ebe36b311f14c363afe3b9c0c1219dbe9962669
Emineozgun <54275270+ozgunemine@users.noreply.github.com>
2020-01-11 18:46:31 +0300

    add controller

---
+++ Controllers/EventController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using net_core_bootcamp_b1_ozgun.DTOs;
+using net_core_bootcamp_b1_ozgun.Services;
+
+namespace net_core_bootcamp_b1_ozgun.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly IEventService _eventService;
+        public EventController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+        [HttpPost("Add")]
+        public IActionResult Add([FromBody]EventAddDto model)
+        {
+            var result = _eventService.Add(model);
+            return Ok(result);
+        }
+    }
+}
---
+++ Services/EventService.cs
@@ -0,0 +1,38 @@
+﻿using net_core_bootcamp_b1_ozgun.DTOs;
+using net_core_bootcamp_b1_ozgun.Models;
+using System;
+using System.Collections.Generic;
+
+namespace net_core_bootcamp_b1_ozgun.Services
+{
+    public interface IEventService
+    {
+        string Add(EventAddDto model);
+    }
+    public class EventService : IEventService
+    {
+        private static readonly IList<Event> data = new List<Event>();
+        public string Add(EventAddDto model)
+        {
+            Event entity = new Event
+            {
+                Id = Guid.NewGuid(),
+                CreatedAt = DateTime.UtcNow
+            };
+            entity.Name = model.Name;
+            entity.StartDate = model.StartDate;
+            entity.FinishDate = model.FinishDate;
+            entity.Address = model.Address;
+            entity.IsFree = model.IsFree;
+            entity.Price = model.Price;
+            entity.Subject = model.Subject;
+            entity.Desc = model.Desc;
+
+            data.Add(entity);
+
+            return ("eklendi");
+        }        
+
+     
+    }
+}
